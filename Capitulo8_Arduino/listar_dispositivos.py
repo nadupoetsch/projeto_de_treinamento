d41675d7aa009896c8b98d6a38902113097bdfe4
Bernardo Poetsch <nadupoetsch@gmail.com>
2023-12-26 13:03:02 -0300

    my first project

--- Capitulo8_Arduino/listar_dispositivos.py
+++ Capitulo8_Arduino/listar_dispositivos.py
@@ -1,6 +1,7 @@
 import serial
 from serial.tools import list_ports
 
-#lista as portas do arduino
+#lista as portas do arduino;
+
 for port in list_ports.comports():
     print('Dispositivo: {} - porta: {} '.format(port.description, port.device))
